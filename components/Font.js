4fc77cc75116e0d709c215d7a4ddb732700636bc
Rodrigo Macedo <rodrigo1987macedo@gmail.com>
2020-02-10 15:34:54 -0300

    _redirects

--- components/Font.js
+++ components/Font.js
@@ -11,36 +11,12 @@ const Fonts = () => {
     weight: 200,
   });
 
-  //const linkRoboto = document.createElement('roboto');
-
-  //linkRoboto.href =
-  //'https://fonts.googleapis.com/css?family=Roboto&display=swap';
-  //linkRoboto.rel = 'stylesheet';
-
-  //document.head.appendChild(linkRoboto);
-
-  //pacifico.load().then(() => {
-  //roboto.load().then(() => {
-  //document.documentElement.classList.add('pacifico');
-  //document.documentElement.classList.add('roboto');
-  //});
-  //});
-
   pacifico
     .load()
     .then(() => {
       roboto.load();
     })
     .catch(() => console.log('failed'));
-
-  //pacifico
-  //.load()
-  //.then(function() {
-  //console.log('Pacifico has loaded.');
-  //})
-  //.catch(function() {
-  //console.log('Pacifico failed to load.');
-  //});
 };
 
 export default Fonts;
